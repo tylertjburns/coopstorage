7eea0317cb33c387653da0babec708e5f36d6a65
tburns <tyler.tj.burns@gmail.com>
2022-06-13 17:23:16 -0500

    updated main to run a Storage

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from coopstorage.my_dataclasses import ResourceUoM, UoMCapacity, location_factory, content_factory, Content
+from coopstorage.storage import Storage
+import tests.uom_manifest as uoms
+import tests.sku_manifest as skus
+import pprint
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main():
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    uom_capacities = frozenset([UoMCapacity(uom=uoms.each, capacity=10)])
+    locs = [location_factory(uom_capacities=uom_capacities, id=str(ii)) for ii in range(10)]
+
+    inv = Storage(locs)
+
+    for ii in range(4):
+        new_cont = content_factory(resource=skus.sku_a, uom=uoms.each, qty=3)
+        inv.add_content(new_cont)
+
+    for ii in range(3):
+        new_cont = content_factory(resource=skus.sku_b, uom=uoms.each, qty=2)
+        inv.add_content(new_cont)
+
+    pprint.pprint(inv)
+    rus = [
+        ResourceUoM(resource=skus.sku_a, uom=uoms.each),
+        ResourceUoM(resource=skus.sku_b, uom=uoms.each),
+     ]
+    pprint.pprint(inv.state.qty_of_resource_uoms(rus))
+
+    inv.remove_content(Content(ResourceUoM(skus.sku_a, uoms.each), 3))
+
+    pprint.pprint(inv.state.InventoryByResourceUom)
+
+if __name__ == "__main__":
+    main()
